aee8ddf2e3013ae649d60f36a2fae03291732050
Umebara <Nakoichi39@outlook.jp>
2023-01-24 23:48:08 +0900

    スクリプトの変更

--- Assets/Ume/UBoxMove.cs
+++ Assets/Ume/UBoxMove.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class UBoxMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Box;
+    public GameObject Open;
+    public Rigidbody RD;
+    bool move;
+    bool Sove;
     void Start()
     {
-        
+        Box = GameObject.Find("MBOX");
+        Open = GameObject.Find("Open");
+        RD=this.Box.GetComponent<Rigidbody>();
+        move = false;
+        Sove = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (move == true && Sove == false)
+        {
+            this.Box.transform.position += new Vector3(0.001f, 0.01f, 0);
+        }
+        if (this.Box.transform.position.y > 13)
+        {
+            Sove = true;
+        }
+        if (this.Box.transform.position.y >= 13)
+        {
+            Destroy(Open);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        move = true;
     }
 }
